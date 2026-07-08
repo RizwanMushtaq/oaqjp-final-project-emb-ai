593cdcf6ffc353da5153ba4bc63af21c7298deb0
Rizwan Mushtaq <rizwanmushtaq15@gmail.com>
2026-07-08 17:05:40 -0400

    updated package

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze: str):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url, json = input_json, headers=headers)
+    
+    formatted_response = json.loads(response.text)
+    emotion_predictions = formatted_response['emotionPredictions']
+    emotions = emotion_predictions[0]['emotion']
+
+    highest_emotion = max(emotions, key=emotions.get)
+    emotions['dominant_emotion'] = highest_emotion
+
+    return emotions
+
+if __name__ == "__main__":
+    emotion_detector("I love this new technology.")
